108c4d257c0a875ff5a675023844641661237b86
Sabrina4455 <89413296+Sabrina4455@users.noreply.github.com>
2022-10-10 16:52:00 -0500

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,5 @@
 for x in range(1,101):
-  if x % 3 == 0 and x % 5 == 0:
-    print("FizzBuzz")  
-  elif x % 3 == 0:
-    print("Fizz")
-  elif x % 5 == 0:
-    print("Buzz")
-  else:
-    print(x)
+  if x % 3 == 0 and x % 5 == 0: print("FizzBuzz")  
+  elif x % 3 == 0: print("Fizz")
+  elif x % 5 == 0: print("Buzz")
+  else: print(x)
